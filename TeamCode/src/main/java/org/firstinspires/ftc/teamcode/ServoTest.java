53812b792b4fc9f9d5f6aec137bfe1bed5f7ff1c
Loveland Nuts & Bolts <robotics5040@gmail.com>
2016-12-27 10:08:56 -0500

    Testing new code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoTest.java
@@ -1,26 +1,43 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by bense on 9/26/2016.
  */
 
+@TeleOp(name = "ServoTest", group = "Testing")
 public class ServoTest extends OpMode {
 
     Servo servo;
+    int count = 0, iteration = 45;
+    boolean aPressed = false;
 
     public void init()
     {
         servo = hardwareMap.servo.get("servo");
+        servo.setPosition(0);
     }
 
     public void loop()
     {
-        if (gamepad1.left_bumper)
-            servo.setPosition(1);
-        if (gamepad1.right_bumper)
-            servo.setPosition(0);
+        if (!gamepad1.dpad_down && !gamepad1.dpad_up)
+            aPressed = false;
+        if ((gamepad1.dpad_down || gamepad1.dpad_up) && !aPressed) {
+            aPressed = true;
+            if (gamepad1.dpad_down)
+                count--;
+            if (gamepad1.dpad_up)
+                count++;
+            if (count * iteration > 360)
+                count--;
+            if (count * iteration < 0)
+                count++;
+            servo.setPosition(((double)count * iteration)/360);
+        }
+        telemetry.addData("Degrees", count * iteration);
+        telemetry.addData("A pressed", aPressed);
     }
 }
